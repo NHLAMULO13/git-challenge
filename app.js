5d2a48cfe07baf96b10adb449b2fb785b8f3b63c
NHLAMULO13 <nhlamulonyalungu@gmail.com>
2025-08-19 04:38:38 +0200

    Changed web background and added buttons

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-function clickMe() {
-  let name = prompt("HELLO! What is your name?");
-  if (name) {
-    let heading = document.querySelector(".welcomingHeading");
-    heading.textContent =
-      "Hello" +
-      name +
-      ". Join me on this tasty journey as we explore new recipes and the joy of sharing meals.Welcome home " +
-      name +
-      "! ";
+for (let i = 1; i <= 6; i++) {
+  const button = document.getElementById(`nextPage${i}`);
+  if (button) {
+    button.addEventListener("click", () => {
+      window.location.href = `index${i + 1}.html`;
+    });
   }
 }
-let button = document
-  .getElementById("clickButton")
-  .addEventsListener("click", clickMe);
+
+const backButton = document.getElementById("backPage");
+if (backButton) {
+  const match = window.location.pathname.match(/index(\d+)\.html$/);
+  if (match) {
+    const currentPage = parseInt(match[1]);
+
+    backButton.addEventListener("click", () => {
+      if (currentPage > 1) {
+        window.location.href = `index${currentPage - 1}.html`;
+      } else {
+        window.location.href = "index.html";
+      }
+    });
+  }
+}
